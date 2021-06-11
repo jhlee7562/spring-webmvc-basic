8f1405ad45a836eaff7609f475f1c7ead76d1dfa
jhlee7562 <78069785+jhlee7562@users.noreply.github.com>
2021-06-11 16:12:55 +0900

    [21.06.11] # Board App v8.1 작성 (카카오 로그인 - 토큰발급)

---
+++ src/main/java/com/spring/mvc/web/member/domain/OAuthValue.java
@@ -0,0 +1,7 @@
+package com.spring.mvc.web.member.domain;
+
+//소셜 로그인 관련 상수 모음
+public interface OAuthValue {
+    String KAKAO_APP_KEY  = "6e4d776b4cc8e59b4aec9123b04f90d6";
+    String KAKAO_REDIRECT_URI = "/auth/kakao";
+}
---
+++ src/main/java/com/spring/mvc/web/member/service/OAuthService.java
@@ -0,0 +1,7 @@
+package com.spring.mvc.web.member.service;
+
+public interface OAuthService {
+
+    //액세스 토큰 발급 메서드
+    String getAccessToken(String authCode) throws Exception;
+}
