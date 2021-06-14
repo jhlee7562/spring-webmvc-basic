e8159cfab1f59f6f48437f01afe55538a1493087
jhlee7562 <78069785+jhlee7562@users.noreply.github.com>
2021-06-14 14:57:47 +0900

    [21.06.14] # Board App v8.2 작성 (카카오 로그인 - 사용자 정보 취득, 로그아웃)

--- src/main/java/com/spring/mvc/web/member/service/KakaoService.java
+++ src/main/java/com/spring/mvc/web/member/service/KakaoService.java
@@ -13,6 +13,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Map;
 
 @Service
 @Log4j2
@@ -42,6 +43,64 @@ public class KakaoService implements OAuthValue, OAuthService {
 
         //응답 데이터를 스트림을 통해 읽음
         return takeResponseData(conn);
+    }
+
+    @Override
+    public Map<String, Object> getKakaoUserInfo(String accessToken) throws Exception {
+
+        String reqUrl = "https://kapi.kakao.com/v2/user/me";
+
+        URL url = new URL(reqUrl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        //요청 메서드 설정
+        conn.setRequestMethod("POST");
+        conn.setDoOutput(true);
+
+        //요청 헤더 정ㅂ모 설정
+        conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()))) {
+
+            //응답 상태코드 200이면 성공
+            int responseCode = conn.getResponseCode();
+            log.info("응답 코드: " + responseCode);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+
+            //응답데이터를 입력스트림으로부터 읽어내기
+            String responseData = br.readLine();
+            log.info("response-data: " + responseData);
+
+            //JSON문자열을 Gson라이브러리를 사용하여 자바 객체로 파싱
+            JsonParser parser = new JsonParser();
+
+            //JsonElement는 자바로 변환된 JSON객체
+            JsonElement element = parser.parse(responseData);
+
+            //필요한 데이터를 json프로퍼티 키를 사용해서 추출
+            //kakao_account 정보 추출
+            JsonObject kakaoAccount = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
+
+            log.info("카카오계정정보 : " + kakaoAccount);
+
+            String nickName = kakaoAccount.get("profile").getAsJsonObject().get("nickname").getAsString();
+            String profileImagPath = kakaoAccount.get("profile").getAsJsonObject().get("profile_image_url").getAsString();
+            String email = kakaoAccount.get("email").getAsString();
+
+            log.info("별명 : " + nickName);
+            log.info("프로필사진경로 : " + profileImagPath);
+            log.info("이메일: " + email);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     private String takeResponseData(HttpURLConnection conn) {
@@ -90,6 +149,35 @@ public class KakaoService implements OAuthValue, OAuthService {
             int responseCode = conn.getResponseCode();
             log.info("응답 코드: " + responseCode);
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //로그아웃 요청
+    public void logout(String accessToken) throws Exception {
+        String reqUrl = "https://kapi.kakao.com/v1/user/logout";
+
+        URL url = new URL(reqUrl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        //요청 메서드 설정
+        conn.setRequestMethod("POST");
+        conn.setDoOutput(true);
+
+        //요청 헤더정보 설정
+        conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+
+        try (BufferedReader br
+                     = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+
+            //응답 상태코드 200이면 성공
+            int responseCode = conn.getResponseCode();
+            log.info("응답 코드(로그아웃): " + responseCode);
+
+            //응답데이터를 입력스트림으로부터 읽어내기
+            String responseData = br.readLine();
+            log.info("logout-response-data: " + responseData);
 
         } catch (Exception e) {
             e.printStackTrace();
